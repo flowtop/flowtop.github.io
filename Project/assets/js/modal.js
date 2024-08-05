415231c4877cb2fa0288a4ceb2496d8dff6f41f7
Mikhail <67060204+flowtop@users.noreply.github.com>
2024-08-05 23:03:49 +0300

    Delete Project directory

--- Project/assets/js/modal.js
+++
@@ -1,9 +0,0 @@
-const modal = document.querySelector(".modal_message");
-
-function openModal() {
-    modal.classList.add("active");
-}
-
-function closeModal() {
-    modal.classList.remove("active");
-}
