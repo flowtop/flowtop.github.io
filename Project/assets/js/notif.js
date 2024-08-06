22393dc2830c4a2b6ffb36e271f595bcfb525ec5
Mikhail <67060204+flowtop@users.noreply.github.com>
2024-08-06 14:26:25 +0300

    Delete Project directory

--- Project/assets/js/notif.js
+++
@@ -1,8 +0,0 @@
-function toggleNotif(notifID, openNotif) {
-    const thisNotif = document.querySelector("#" + notifID);
-    if (openNotif) {
-        thisNotif.classList.add("active");
-    } else {
-        thisNotif.classList.remove("active");
-    }
-}
